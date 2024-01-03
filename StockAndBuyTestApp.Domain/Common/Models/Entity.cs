562cff7f93fe729a26e4db76b6a3e4294ca1735e
dima-ma <dmytro.m.forwork@gmail.com>
2024-01-03 20:22:17 +0200

    Create base classes to build a domain model

---
+++ StockAndBuyTestApp.Domain/Common/Models/Entity.cs
@@ -0,0 +1,30 @@
+﻿namespace StockAndBuyTestApp.Domain.Common.Models;
+
+public class Entity<TId> : IEquatable<Entity<TId>>
+    where TId : notnull
+{
+    public TId Id { get; }
+
+    public Entity(TId id)
+    {
+        Id = id;
+    }
+
+    public override bool Equals(object? obj) => Equals(obj as Entity<TId>);
+
+    public static bool operator ==(Entity<TId> obj1, Entity<TId> obj2) => Equals(obj1, obj2);
+
+    public static bool operator !=(Entity<TId> obj1, Entity<TId> obj2) => !Equals(obj1, obj2);
+
+    public bool Equals(Entity<TId>? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return EqualityComparer<TId>.Default.Equals(Id, other.Id);
+    }
+
+    public override int GetHashCode()
+    {
+        return EqualityComparer<TId>.Default.GetHashCode(Id);
+    }
+}
---
+++ StockAndBuyTestApp.Domain/Common/Models/ValueObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace StockAndBuyTestApp.Domain.Common.Models;
+
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    public abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj) => Equals(obj as ValueObject);
+
+    public override int GetHashCode()
+        => GetEqualityComponents()
+            .Select(x => x?.GetHashCode() ?? 0)
+            .Aggregate((x, y) => x * y);
+
+    public static bool operator ==(ValueObject obj1, ValueObject obj2) => Equals(obj1, obj2);
+
+    public static bool operator !=(ValueObject obj1, ValueObject obj2) => !Equals(obj1, obj2);
+
+    public bool Equals(ValueObject? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+
+        return GetEqualityComponents()
+            .SequenceEqual(other.GetEqualityComponents());
+    }
+}
